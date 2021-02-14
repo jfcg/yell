1862e5e61502aece2639385e795a9c6c1f3ac355
Serhat Şevki Dinçer <jfcgauss@gmail.com>
2021-02-14 19:52:33 +0300

    unexport Logger fields, add New()/SetMinLevel()/UpdateWriter()

--- yell.go
+++ yell.go
@@ -46,10 +46,8 @@ var UTC = false
 //  	"github.com/jfcg/yell"
 //  )
 //
-//  // log to stdout with warn or higher severity (for example). Update Logger.Slevel to
-//  // change log severity level or disable logging. Message list given to Info() etc.
-//  // must not be empty or end with a newline.
-//  var Logger = yell.Logger{": mypkg:", os.Stdout, yell.Swarn}
+//  // log to stdout with warn or higher severity (for example).
+//  var Logger = yell.New(": mypkg:", os.Stdout, yell.Swarn)
 //
 //  // Info tries to log message list with info severity
 //  func Info(msg ...interface{}) error {
@@ -71,21 +69,42 @@ var UTC = false
 //  // Fatal tries to log message list with fatal severity and panics
 //  func Fatal(msg ...interface{}) (err error) {
 //  	err = Logger.Log(Sfatal, msg...)
-//  	pm := Logger.Package[2:] + yell.Sname[yell.Sfatal]
+//  	pm := Logger.Name() + yell.Sname[yell.Sfatal]
 //  	if err != nil {
 //  		pm += err.Error()
 //  	}
 //  	panic(pm)
 //  }
 type Logger struct {
-	// Package or application name. It must be of the form ": name:"
-	Package string
+	// name of package or application, must be of the form ": mypkg:"
+	name string
 
-	// Writer is used to log messages, can also be sync.Locker. It must not be nil.
-	Writer io.Writer
+	// writer is used to log messages, can also be sync.Locker, must not be nil
+	writer io.Writer
 
-	// Slevel is minimum severity for logging
-	Slevel Severity
+	// minLevel is minimum severity for logging
+	minLevel Severity
+}
+
+func isSpace(b byte) bool {
+	return b == ' ' || b == '\t' || b == '\n' || b == '\r'
+}
+
+// New creates a Logger with package/application name (must be of the form ": mypkg:"),
+// writer to log (which can also implement sync.Locker to protect logging) and minimum
+// severity level to log. Panics if arguments are invalid.
+func New(name string, writer io.Writer, minLevel Severity) Logger {
+	l := len(name) - 1
+	if l < 3 || name[0] != ':' || name[1] != ' ' || isSpace(name[2]) ||
+		isSpace(name[l-1]) || name[l] != ':' || writer == nil || minLevel > Snolog {
+		panic("Invalid arguments to yell.New()")
+	}
+	return Logger{name, writer, minLevel}
+}
+
+// Name of Logger, skipping ": "
+func (lg *Logger) Name() string {
+	return lg.name[2:]
 }
 
 // for not importing sync
@@ -94,13 +113,46 @@ type locker interface {
 	Unlock()
 }
 
+// UpdateWriter tries to update Logger's writer. If both old & new writers implement
+// sync.Locker, they must resolve to the same locker. Otherwise UpdateWriter refuses
+// to update because old locker could still be in use in Log() calls while we update.
+// Returns true on successful update.
+func (lg *Logger) UpdateWriter(writer io.Writer) (success bool) {
+	if writer == nil {
+		return false
+	}
+
+	// see if writers are also sync.Locker
+	if lc, ok := lg.writer.(locker); ok {
+
+		if lc2, ok := writer.(locker); ok && lc != lc2 {
+			return false // different lockers
+		}
+
+		lc.Lock()
+		defer lc.Unlock()
+	}
+
+	lg.writer = writer
+	return true
+}
+
+// SetMinLevel sets minimum severity level for logging
+func (lg *Logger) SetMinLevel(level Severity) {
+	if level > Snolog {
+		lg.minLevel = Snolog
+	} else {
+		lg.minLevel = level
+	}
+}
+
 // Log records message list to Logger if level is severe enough for Logger and the list
 // is not empty. Message list must not end with a newline. Log tries to include request
 // location (file.go:line) in records, so it must be called as described in Logger doc.
-// If Logger.Writer also implements sync.Locker, Lock/Unlock is used to protect logging.
+// If Logger.writer also implements sync.Locker, Lock/Unlock is used to protect logging.
 func (lg *Logger) Log(level Severity, msg ...interface{}) (err error) {
 
-	if !(len(msg) > 0 && lg.Slevel <= level && level < Snolog) {
+	if !(len(msg) > 0 && lg.minLevel <= level && level < Snolog) {
 		return // empty msg or ignored level
 	}
 
@@ -109,7 +161,7 @@ func (lg *Logger) Log(level Severity, msg ...interface{}) (err error) {
 	if UTC {
 		now = now.UTC()
 	}
-	prem := now.Format(TimeFormat) + lg.Package + Sname[level]
+	prem := now.Format(TimeFormat) + lg.name + Sname[level]
 
 	// try to locate request location
 	_, file, line, ok := runtime.Caller(2)
@@ -121,23 +173,20 @@ func (lg *Logger) Log(level Severity, msg ...interface{}) (err error) {
 	// prepend prem
 	msg = append([]interface{}{prem}, msg...)
 
-	// see if Writer is also a sync.Locker
-	if lc, ok := lg.Writer.(locker); ok {
+	// see if writer is also a sync.Locker
+	if lc, ok := lg.writer.(locker); ok {
 
 		lc.Lock() // lock just before logging
 		defer lc.Unlock()
 	}
 
-	fmt.Fprintln(lg.Writer, msg...)
+	fmt.Fprintln(lg.writer, msg...)
 	return
 }
 
-// Default logger
+// Default logger utilizes os.Args[0] for name, os.Stdout as writer, with warn severity.
 var Default = Logger{
-	// application name
 	": " + filepath.Base(os.Args[0]) + ":",
-
-	// log to standard output with warn or higher severity
 	os.Stdout, Swarn}
 
 // Info tries to log message list with info severity to Default logger
@@ -158,7 +207,7 @@ func Error(msg ...interface{}) error {
 // Fatal tries to log message list with fatal severity to Default logger and panics
 func Fatal(msg ...interface{}) (err error) {
 	err = Default.Log(Sfatal, msg...)
-	pm := Default.Package[2:] + Sname[Sfatal]
+	pm := Default.Name() + Sname[Sfatal]
 	if err != nil {
 		pm += err.Error()
 	}
